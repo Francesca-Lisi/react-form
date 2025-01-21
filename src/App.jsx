183db5fcc1fe1073de5eab9b09a432fd40ec40ac
Francesca-Lisi <francescalisi2143@gmail.com>
2025-01-21 21:14:40 +0100

    feat: add form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='container my-5'>
+      <h2 className='my-4'>I miei Articoli</h2>
+      <form action="#">
+        <div className='input-group mb-3'>
+          <input
+            type="text"
+            placeholder='Inserire nome Articolo'
+            className='form-control'
+          />
+          <button
+            className='btn btn-outline-dark'
+            type='submit'
+          >Invia</button>
+        </div>
+      </form>
+
+      <div className='card p-3'>
+        <ul className="list-group list-group">
+          <li className="list-group-item d-flex justify-content-between align-items-start">
+            <div className="ms-2 me-auto">
+              <h5 className="fw-bold">Titolo</h5>
+              {/* <span>Altre info</span> */}
+            </div>
+            <div className='align-items-center'>
+              <i class="fa-solid fa-trash-can m-2"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
